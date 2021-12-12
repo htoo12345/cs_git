9eae7ad1cbe2d5f4d2602ed05cabc6b8dfe2d3e0
Htoo Aung Lynn <htooaunglynn6@gmail.com>
2021-12-12 19:09:26 +0630

    First Commit

---
+++ Visual_studio/C# SA B1/HelloWorldGUI/HelloWorldGUI/SimpleCalculatorUI.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace HelloWorldGUI
+{
+    public partial class SimpleCalculatorUI : Form
+    {
+        public SimpleCalculatorUI()
+        {
+            InitializeComponent();
+        }
+
+        private void txtAddition_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                double firstNumber = Convert.ToDouble(txtFirstNumber.Text);
+                double secondNumber = Convert.ToDouble(txtSecondNumber.Text);
+                double result = firstNumber + secondNumber;
+                txtResult.Text = result.ToString();
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("Invalid values!","Error occur!",MessageBoxButtons.OK,MessageBoxIcon.Error);
+            }
+        }
+
+        private void txtClean_Click(object sender, EventArgs e)
+        {
+            txtFirstNumber.Text = String.Empty;
+            txtSecondNumber.Text = String.Empty;
+            txtResult.Text= String.Empty;
+        }
+    }
+}
