6812959fd344d2cb57638c5a2870f477c21f64d2
Htoo Aung Lynn <htooaunglynn6@gmail.com>
2021-12-20 18:03:36 +0630

    First Commit

--- Visual_studio/Sololearn/Tutorial/Tutorial/Program.cs
+++ Visual_studio/Sololearn/Tutorial/Tutorial/Program.cs
@@ -1,16 +1,18 @@
-﻿//using System;
-//using System.Collections.Generic;
-//using System.Linq;
-//using System.Text;
-//using System.Threading.Tasks;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
 namespace Tutorial
 {
-    internal class Program
+    public class Program
     {
         static void Main(string[] args)
         {
-            ParameterAndArgument.Call();
+            PolymorphismDemo.Poly();
         }
     } // end of Program class
+
+   
 } // end of tutorial
