6241531832589847b7fb78cc0a82df40c018ce7f
htoo12345 <htooaunglynn6@gmail.com>
2022-06-02 19:38:26 +0630

    add

--- Visual_code/CV/CV.cs
+++ Visual_code/CV/CV.cs
@@ -8,14 +8,19 @@ class CV
     static void Receive()
     {
         Console.Write("How many value you input?  ");
+
         int inputNumberLength=int.Parse(Console.ReadLine());
+
         int[] valueArray=new int[inputNumberLength];
+
         for(int i=0;i<valueArray.Length;i++)
         {
             Console.Write("Enter {0} number is: ",i+1);
             valueArray[i]=int.Parse(Console.ReadLine());
         }  
-       Console.Write("Input number are ");
+
+        Console.Write("Input number are : ");
+
         for(int j=0;j<valueArray.Length;j++)
         {
             if(j<valueArray.Length-1)
@@ -27,14 +32,20 @@ class CV
                 Console.Write(valueArray[j]);
             }
         }
-         Console.WriteLine();
-            int total=0;
-            for(int s=0;s<valueArray.Length;s++)
-            {
-                total+=valueArray[s];
-            }
-            Console.WriteLine("Total number is {0}",total);
+
+        Console.WriteLine();
+
+        int total=0;
+
+        for(int s=0;s<valueArray.Length;s++)
+        {
+            total+=valueArray[s];
+        }
+
+        Console.WriteLine("Total number is {0}",total);
+
         int Maximum=valueArray[0];
+
         for(int m=1;m<valueArray.Length;m++)
         {
             if(Maximum<valueArray[m])
